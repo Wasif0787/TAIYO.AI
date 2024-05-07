97131fb71d35aa6ca60a5a094407d8f9cc74db25
Wasif0787 <wasifhussain787@gmail.com>
2024-05-07 16:33:56 +0530

    Added chart graph

--- src/Pages/Chart.tsx
+++ src/Pages/Chart.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Map from "../components/Map";
+import Graph from "../components/Graph";
+const queryClient = new QueryClient();
 
 const Chart = () => {
+    const [showMap, setShowMap] = useState(true);
     return (
-        <div>Chart</div>
+        <div className=' '>
+            <div className="w-full flex flex-col items-center">
+                <div className=" p-2 rounded-md shadow-md mb-2">
+                    <button
+                        className={`text-xl md:text-2xl font-semibold py-2 px-4 mb-2 md:mb-4 rounded-md focus:outline-none ${showMap ? 'bg-[#f75990] text-white' : 'text-gray-800 hover:bg-pink-300'}`}
+                        onClick={() => setShowMap(true)}
+                    >
+                        Maps
+                    </button>
+                    <button
+                        className={`text-xl md:text-2xl font-semibold py-2 px-4 rounded-md focus:outline-none ${!showMap ? 'bg-[#f75990] text-white' : 'text-gray-800 hover:bg-pink-300'}`}
+                        onClick={() => setShowMap(false)}
+                    >
+                        Graph
+                    </button>
+                </div>
+            </div>
+            <QueryClientProvider client={queryClient}>
+                <div className="p-4 ">
+                    <div className="">
+                        {showMap ? <Map /> : <Graph />}
+                    </div>
+                </div>
+            </QueryClientProvider>
+        </div>
     )
 }
 
---
+++ src/components/Graph.tsx
@@ -0,0 +1,87 @@
+import { LineChart } from "@mui/x-charts";
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import Checkbox from "@mui/material/Checkbox";
+
+const Graph = () => {
+    const [casesShow, setCasesShow] = useState(true);
+    const [recoveredShow, setRecoveredShow] = useState(false);
+    const [deathsShow, setDeathsShow] = useState(false);
+
+    const { isPending, error, data } = useQuery({
+        queryKey: ["repoData"],
+        queryFn: () =>
+            fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=all#").then(
+                (res) => res.json()
+            ),
+    });
+    if (isPending) return <>"Loading..."</>;
+
+    if (error) return <>"An error has occurred: " + {error.message}</>;
+    return (
+        <div className="">
+            <h1 className="text-center ">
+                COVID-19 Cases Fluctuations
+            </h1>
+            <LineChart
+                height={500}
+                margin={{ left: 130, right: 130, top: 130 }}
+                series={[
+                    casesShow
+                        ? {
+                            data: Object.values(data.cases),
+                            label: "Cases",
+                            color: "blue",
+                        }
+                        : { data: [] },
+                    deathsShow
+                        ? {
+                            data: Object.values(data.deaths),
+                            label: "Deaths",
+                            color: "red",
+                        }
+                        : { data: [] },
+                    recoveredShow
+                        ? {
+                            data: Object.values(data.recovered),
+                            label: "Recovered",
+                            color: "green",
+                        }
+                        : { data: [] },
+                ]}
+                xAxis={[{ scaleType: "point", data: Object.keys(data.cases) }]}
+            />
+            <div className="flex justify-center text-center">
+                <div>
+                    Cases
+                    <Checkbox
+                        checked={casesShow}
+                        onChange={() => {
+                            setCasesShow(casesShow ? false : true);
+                        }}
+                    />
+                </div>
+                <div>
+                    Deaths
+                    <Checkbox
+                        checked={deathsShow}
+                        onChange={() => {
+                            setDeathsShow(deathsShow ? false : true);
+                        }}
+                    />
+                </div>
+                <div>
+                    Recovered
+                    <Checkbox
+                        checked={recoveredShow}
+                        onChange={() => {
+                            setRecoveredShow(recoveredShow ? false : true);
+                        }}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Graph
